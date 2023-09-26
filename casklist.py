c48c5090ad8874848075a4b73df5119cfcf96bb8
Kuang Jiang <kj@KKOpacity.local>
2023-09-26 18:46:21 -0400

    Initial commit

---
+++ casklist.py
@@ -0,0 +1,122 @@
+# Cask List File
+# Version 1.0
+# A Modified Linked List for Storage
+
+##################################################
+# Libraries
+##################################################
+
+
+##################################################
+# Variable Definition
+##################################################
+class Node:
+    def __init__(self, name, index=None, position=None, speed=None, range=None, contact=None, active=None, image=None):
+        self.name = name
+        self.next = None
+        # Format Data
+        self.index = index
+        self.position = position
+        self.speed = speed
+        self.range = range
+        self.contact = contact
+        self.contact_origin = tuple(tuple(i) for i in contact) if contact is not None else None
+        self.active = active
+        self.image = image
+
+
+class CaskList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data, index, position, speed, range, contact, active, image):
+        new_node = Node(name=data, index=index, position=position, speed=speed, range=range, contact=contact, active=active, image=image)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def prepend(self, data, index, position, speed, range, contact, active, image):
+        new_node = Node(name=data, index=index, position=position, speed=speed, range=range, contact=contact, active=active, image=image)
+        new_node.next = self.head
+        self.head = new_node
+
+    def delete(self, data):
+        if not self.head:
+            return
+        if self.head.name == data:
+            self.head = self.head.next
+            return
+        current = self.head
+        while current.next:
+            if current.next.name == data:
+                current.next = current.next.next
+                return
+            current = current.next
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.name, end=" -> ")
+            current = current.next
+        print("None")
+
+    def length(self):
+        count = 0
+        current = self.head
+        while current:
+            count += 1
+            current = current.next
+        return count
+
+    def search_index(self, index):
+        current = self.head
+        while current:
+            if current.index == index:
+                return current
+            current = current.next
+        return False
+
+    def search_active(self):
+        current = self.head
+        while current:
+            if current.active:
+                return current
+            current = current.next
+        return self.head
+
+
+##################################################
+# Class Prototype
+##################################################
+
+
+##################################################
+# Function Prototype
+##################################################
+
+
+##################################################
+# Main Function
+##################################################
+def main():
+    print("Hello World!")
+    my_list = CaskList()
+    my_list.append(1)
+    my_list.append(2)
+    my_list.append(3)
+    my_list.prepend(0)
+    my_list.display()  # Output: 0 -> 1 -> 2 -> 3 -> None
+
+    my_list.delete(2)
+    my_list.display()  # Output: 0 -> 1 -> 3 -> None
+
+
+##################################################
+# Main Function Runner
+##################################################
+if __name__ == "__main__":
+    main()
---
+++ game.py
@@ -0,0 +1,226 @@
+# Top Down Shooter Game
+# Version 2.0
+# A Simple Top Down Shooter for Raiden Mockup
+# Add: Multiple Enemy with CaskList
+
+##################################################
+# Libraries
+##################################################
+import pygame
+import random
+import time
+
+from casklist import CaskList
+
+##################################################
+# Variable Definition
+##################################################
+# Constants
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 800
+# Player Modification
+PLAYER_SPEED = 0.5
+PLAYER_SIZE = 60  # Player Image Size 50x50 Pixel
+# Bullet Modification
+BULLET_SPEED_BASE = 0.8  # Bullet Moving Speed
+BULLET_EXPLOSION_RANGE = 30  # Bullet Explosion Range
+BULLET_TYPE = 2  # Total Number of Bullet
+# Enemy Modification
+ENEMY_SIZE = 50  # Enemy Image Size 50x50 Pixel
+ENEMY_SPEED_MAX = 100  # /1000 -> Max of 0.1
+ENEMY_NUMBER = 5  # On Screen Enemy Number
+ENEMY_TYPE = 2  # Total Number of Enemy
+ENEMY_SPAWN = (50, 200)
+# Colors
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+# Settings
+# Initialize Pygame
+pygame.init()
+# Create Screen
+screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Top-Down Shooter")
+# Player Value
+player_img = pygame.image.load('resources/player.png')
+player_x = SCREEN_WIDTH // 2 - PLAYER_SIZE / 2  # Middle of Screen
+player_y = SCREEN_HEIGHT - 128  # Manually Set
+player_x_change = 0
+# Explosion
+EXPLOSION_TIME = 0.5  # second
+explosion_img = pygame.image.load('resources/explosion.png')
+# Enemy
+enemy_img = []
+for i in range(ENEMY_TYPE):
+    file = 'resources/enemy' + str(i) + '.png'
+    enemy_img.append(pygame.image.load(file))
+# Enemy Cask - [x, y, type, speed]
+enemy_list = CaskList()
+for num_enemy in range(ENEMY_NUMBER):
+    enemy_list.append(data='enemy' + str(num_enemy), index=num_enemy,
+                      position=[random.randint(0, SCREEN_WIDTH - ENEMY_SIZE),
+                                random.randint(ENEMY_SPAWN[0], ENEMY_SPAWN[1])],
+                      speed=random.randint(1, ENEMY_SPEED_MAX) / 1000,
+                      active=True,
+                      image=enemy_img[random.randint(0, ENEMY_TYPE - 1)],
+                      range=None,
+                      contact=None)
+# Bullet
+BULLET_ORIGIN_X = player_x
+BULLET_ORIGIN_Y = player_y
+bullet_state = 'ready'
+# Bullet Cask
+bullet_list = CaskList()
+bullet_list.append(data='bullet0', index=0, position=[BULLET_ORIGIN_X, BULLET_ORIGIN_Y],
+                   speed=BULLET_SPEED_BASE,
+                   range=BULLET_EXPLOSION_RANGE,
+                   contact=[[BULLET_ORIGIN_X + 28, BULLET_ORIGIN_Y], [BULLET_ORIGIN_X + 32, BULLET_ORIGIN_Y]],
+                   active=True,
+                   image=pygame.image.load('resources/bullet0.png'))
+bullet_list.append(data='bullet1', index=1, position=[BULLET_ORIGIN_X, BULLET_ORIGIN_Y],
+                   speed=BULLET_SPEED_BASE,
+                   range=BULLET_EXPLOSION_RANGE * 0.8,
+                   contact=[[BULLET_ORIGIN_X + 18, BULLET_ORIGIN_Y + 2], [BULLET_ORIGIN_X + 42, BULLET_ORIGIN_Y + 2]],
+                   active=False,
+                   image=pygame.image.load('resources/bullet1.png'))
+
+
+##################################################
+# Class Prototype
+##################################################
+
+
+##################################################
+# Function Prototype
+##################################################
+def player(x, y):
+    global screen, player_img
+    screen.blit(player_img, (x, y))
+
+
+def enemy(enemy_node):
+    global screen
+    screen.blit(enemy_node.image, (enemy_node.position[0], enemy_node.position[1]))
+
+
+def fire_bullet(bullet):
+    global bullet_state, screen
+    bullet_state = 'fire'
+    screen.blit(bullet.image, (bullet.position[0], bullet.position[1] - 10))
+
+
+def collide(enemy_x, enemy_y, bullet_x, bullet_y, explosion_range):
+    distance = (((enemy_x + ENEMY_SIZE / 2) - (bullet_x + PLAYER_SIZE / 2)) ** 2 +
+                ((enemy_y + ENEMY_SIZE / 2) - (bullet_y + PLAYER_SIZE / 2)) ** 2
+                ) ** 0.5
+    return distance < explosion_range
+
+
+##################################################
+# Main Function
+##################################################
+def main():
+    # Global Variable
+    global EXPLOSION
+    global screen
+    global player_x, player_y, player_x_change
+    global enemy_list
+    global bullet_list, bullet_state
+    # Running Game
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+        # FInd Active Bullet
+        bullet = bullet_list.search_active()
+        # Obtain Keyboard Event
+        for event in pygame.event.get():
+            # Event of Quiting
+            if event.type == pygame.QUIT:
+                running = False
+            # Event of Key Press
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player_x_change = -PLAYER_SPEED
+                elif event.key == pygame.K_RIGHT:
+                    player_x_change = PLAYER_SPEED
+                elif event.key == pygame.K_SPACE and bullet_state == 'ready':
+                    # Bullet Fire at Current Player_X
+                    bullet.position[0] = player_x
+                    # Bullet contact set
+                    for item in bullet.contact:
+                        item[0] = player_x
+                    fire_bullet(bullet=bullet)
+            # Event of Key Release
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    player_x_change = 0
+        # Player Movement
+        player_x += player_x_change
+        # Check Boundary
+        if player_x < 0:
+            player_x = 0
+        elif player_x >= SCREEN_WIDTH - 64:
+            player_x = SCREEN_WIDTH - 64
+        # Movement of Each Enemy
+        for enemy_index in range(enemy_list.length()):
+            current_enemy = enemy_list.search_index(index=enemy_index)
+            current_enemy.position[0] += current_enemy.speed
+            if current_enemy.position[0] >= SCREEN_WIDTH - ENEMY_SIZE:
+                current_enemy.position[0] = 0
+                current_enemy.position[1] = random.randint(ENEMY_SPAWN[0], ENEMY_SPAWN[1])
+        # Bullet movement
+        if bullet_state == "fire":
+            # Bullet Position
+            bullet.position[1] -= bullet.speed
+            for item in bullet.contact:
+                item[1] -= bullet.speed
+            fire_bullet(bullet=bullet)
+            # Reset Bullet
+            if bullet.position[1] <= 0:
+                bullet_state = "ready"
+                bullet.position[1] = player_y
+                for index in range(len(bullet.contact_origin)):
+                    bullet.contact[index] = list(bullet.contact_origin[index])
+        # Collision
+        for enemy_index in range(enemy_list.length()):
+            current_enemy = enemy_list.search_index(enemy_index)
+            for contact_point in bullet.contact:
+                collided = collide(enemy_x=current_enemy.position[0], enemy_y=current_enemy.position[1],
+                                   bullet_x=contact_point[0], bullet_y=contact_point[1],
+                                   explosion_range=bullet.range)
+                if collided:
+                    # Explosion
+                    if current_enemy.active:
+                        current_enemy.speed = 0  # Stop Moving
+                        current_enemy.image = explosion_img
+                        current_enemy.range = time.time()
+                        current_enemy.active = False
+                    # Bullet Reset after Collision
+                    bullet.position[1] = SCREEN_HEIGHT - 64
+                    bullet_state = 'ready'
+                    for index in range(len(bullet.contact_origin)):
+                        bullet.contact[index] = list(bullet.contact_origin[index])
+                if current_enemy.range is not None:
+                    # Reset Enemy after Explosion of EXPLOSION_TIME
+                    if time.time() - current_enemy.range >= EXPLOSION_TIME:
+                        current_enemy.position = [0, random.randint(ENEMY_SPAWN[0], ENEMY_SPAWN[1])]
+                        current_enemy.speed = random.randint(1, ENEMY_SPEED_MAX) / 1000
+                        current_enemy.image = enemy_img[random.randint(0, ENEMY_TYPE - 1)]
+                        current_enemy.active = True
+                        current_enemy.range = None
+        # Update New Location
+        player(player_x, player_y)
+        for enemy_index in range(enemy_list.length()):
+            current_enemy = enemy_list.search_index(index=enemy_index)
+            enemy(enemy_node=current_enemy)
+        # Update Display
+        pygame.display.update()
+        # Break Point
+    # Quit
+    pygame.quit()
+
+
+##################################################
+# Main Function Runner
+##################################################
+if __name__ == "__main__":
+    main()
